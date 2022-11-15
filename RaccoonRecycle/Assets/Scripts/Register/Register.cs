58c4d96bb47c7e6055dd6d7c78ae1ffb737ac5f6
Hajtó Kornél <72157420+Lxndr01@users.noreply.github.com>
2022-11-15 09:35:38 +0100

    Updated Login/Register

--- RaccoonRecycle/Assets/Scripts/Register/Register.cs
+++ RaccoonRecycle/Assets/Scripts/Register/Register.cs
@@ -72,7 +72,7 @@ public class Register : MonoBehaviour
         form.AddField("aUsername", username);
 
 
-        var request = UnityWebRequest.Post(registerEndpoint, form);
+        var request = UnityWebRequest.Post("http://192.168.15.154:18102/api/register", form);
         var handler = request.SendWebRequest();
 
         float startTime = 0f;
