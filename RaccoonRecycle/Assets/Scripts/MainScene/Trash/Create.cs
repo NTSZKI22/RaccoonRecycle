670f77cfd8c78442cfaeeb53f79f55a75eeec0f2
BojszaReka <113019065+BojszaReka@users.noreply.github.com>
2022-11-27 14:11:06 +0100

    movemen&selling

--- RaccoonRecycle/Assets/Scripts/MainScene/Trash/Create.cs
+++ RaccoonRecycle/Assets/Scripts/MainScene/Trash/Create.cs
@@ -5,46 +5,44 @@ using UnityEngine;
 public class Create : MonoBehaviour
 {
     
-    public Rigidbody2D trashPrefab;
-    public GameObject parent;
+    public Rigidbody2D trashPrefab; //pbulikus változó a létrehozni kívánt szemétnek
+    public GameObject parent; //publikus változó a szemét szülõobjektumához
     
-
+    //ideiglenes változók a szemét mûködéséhez
     public int frequency;
     public int speed;
     public int value;
 
-    private Vector2 location;
-    private Rigidbody2D rb;
+    private Vector2 location; //vector2 változó egy helyzethez
+    private Rigidbody2D rb; //fizikával rendelkezõ objektum változó
 
-    void Start()
+    void Start() //metódus lefut a játék indulásakor
     {
-        location = GameObject.Find("Generator").transform.position;
-        Spawn();
-        StartCoroutine(Flow());
+        location = GameObject.Find("Generator").transform.position; //location változó megkapja a Generator objektum helyzetét adatként
+        Spawn(); //meghívja a spawn metódust
+        StartCoroutine(Flow()); //meghívja a flow metódust
     }
 
-    void Update()
+    void Update() //minden képrissitésnél lefut
     {
 
     }
 
-    private void Spawn()
-    {
-        //GameObject a = Instantiate(trashPrefab) as GameObject;
-        Rigidbody2D b = Instantiate(trashPrefab) as Rigidbody2D;
-        b.transform.position = location;
-        b.transform.parent = parent.transform;
-        rb = b; //.AddComponent<Rigidbody2D>() as Rigidbody2D;
-        rb.isKinematic = true;
-        rb.velocity = new Vector2(speed, 0);
+    private void Spawn() //metódus, lefutásával létrehoz szemét objektumokat
+    { 
+        Rigidbody2D rb = Instantiate(trashPrefab) as Rigidbody2D; //létrehoz egy rigidbody2d-t a trashPrefab-ból
+        rb.transform.position = location; // rb helyzete a generator helyzete lesz
+        rb.transform.parent = parent.transform; //rb szülõ objektumát beállítja
+        rb.isKinematic = true; //nemtudom pontosan mit csinál, de kell hogy mûködjön, függ tõle az objektum fizikája
+        rb.velocity = new Vector2(speed, 0); //rb mozgása: megindul ebbe az irányba
     }
     
-    IEnumerator Flow()
+    IEnumerator Flow() //metódus, lefutásával folyamatos a szemetek létrehozása
     {
-        while (true)
+        while (true) //végtelen ciklus
         {
-            yield return new WaitForSeconds(frequency);
-            Spawn();
+            yield return new WaitForSeconds(frequency); //vár annyi másodpercet, amennyi frequency értéke
+            Spawn(); //meghívja a spawn metódust
         }
 
     }
--- RaccoonRecycle/Assets/Scripts/MainScene/Trash/Path.cs
+++ RaccoonRecycle/Assets/Scripts/MainScene/Trash/Path.cs
@@ -4,30 +4,68 @@ using UnityEngine;
 
 public class Path : MonoBehaviour
 {
+    public int speed; //ideiglenes véltozó, sebessége az objektumnak
+    Rigidbody2D rb; //fizikával rendelkezõ objektum változó
 
-    //public GameObject ez;
-    public int speed;
+    Selling sellingScript;
 
-    void OnTriggerEnter2D(Collider2D other)
+    void Start() //induláskor lefut
     {
-        //if the player collider hits a gameobject with tag "Laser"
-        //then the gameobject the script is attached to will be destroyed
-        if (other.gameObject.tag == "PetBottle" && this.gameObject.tag == "Collider_PB")
+        rb = GetComponent<Rigidbody2D>(); //rb-t deklaráljuk mint a jelenlegi fizikával rendelkezõ objektum
+        sellingScript = GameObject.FindGameObjectWithTag("SellingScript").GetComponent<Selling>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other) //akkor fut le, amikor a szemét objektum ütközik valamivel
+    {
+        
+        if (other.gameObject.tag == "Collider_PB" && gameObject.tag == "PetBottle")//ha Collider_PB-vel ütközik PetBottle
         {
-            //other.transform.velocity = new Vector2(0, speed);
+            rb.velocity = new Vector2(0, speed); //irányt vált
         }
-        if (other.gameObject.tag == "Box" && this.gameObject.tag == "Collider_BX")
+
+        if (other.gameObject.tag == "Collider_BX" && gameObject.tag == "Box") //ha Collider_BX-vel ütközik Box
         {
-            
+            rb.velocity = new Vector2(0, speed); //irányt vált
         }
-        if (other.gameObject.tag == "Glass" && this.gameObject.tag == "Collider_GL")
+
+        if (other.gameObject.tag == "Collider_GL" && gameObject.tag == "Glass") //ha Collider_GL-vel ütközik Glass
         {
-           
+            rb.velocity = new Vector2(0, speed); //irányt vált
         }
-        if (other.gameObject.tag == "Battery" && this.gameObject.tag == "Collider_BY")
+
+        if (other.gameObject.tag == "Collider_BY" && gameObject.tag == "Battery") //ha Collider_BY-vel ütközik Battery
         {
-            
+            rb.velocity = new Vector2(0, speed); //irányt vált
         }
-        //
+
+        if (other.gameObject.tag == "DefSeller") //ha a defseller-el ütközik
+        {
+            sellingScript.normalSelling(); //meghívja a sellingscript normalselling metódusát
+            Destroy(gameObject);
+        }
+
+        if(other.gameObject.tag == "Seller_PB")
+        {
+            sellingScript.sellingPB();
+            Destroy(gameObject);
+        }
+
+        if (other.gameObject.tag == "Seller_BX")
+        {
+            sellingScript.sellingBX();
+            Destroy(gameObject);
+        }
+
+        if (other.gameObject.tag == "Seller_GL")
+        {
+            sellingScript.sellingGL();
+            Destroy(gameObject);
+        }
+
+        if (other.gameObject.tag == "Seller_BY")
+        {
+            sellingScript.sellingBY();
+            Destroy(gameObject);
+        }
     }
 }
